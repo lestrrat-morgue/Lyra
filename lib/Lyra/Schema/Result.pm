495f0b0d85a2580bbda82f66dff4887e4a267643
lestrrat <lestrrat+github@gmail.com>
2010-03-09 17:35:27 +0900

    add a schema

---
+++ lib/Lyra/Schema/Result.pm
@@ -0,0 +1,18 @@
+package Lyra::Schema::Result;
+use strict;
+use warnings;
+use base qw(DBIx::Class);
+
+__PACKAGE__->mk_classdata(engine => 'InnoDB');
+__PACKAGE__->mk_classdata(charset => 'UTF-8');
+
+sub sqlt_deploy_hook {
+    my ($self, $sqlt_table) = @_;
+
+    $sqlt_table->extra->{mysql_table_type} = $self->engine;
+    $sqlt_table->extra->{mysql_charset}    = $self->charset;
+    return;
+}
+
+1;
+
---
+++ lib/Lyra/Schema/Result/ClickLog.pm
@@ -0,0 +1,30 @@
+package Lyra::Schema::Result::ClickLog;
+use strict;
+use warnings;
+use base qw(Lyra::Schema::Result);
+
+__PACKAGE__->load_components( qw(TimeStamp Core) );
+__PACKAGE__->table('lyra_click_log');
+__PACKAGE__->add_columns(
+    id => {
+        data_type => 'CHAR',
+        size      => 10,
+        is_nullable => 0,
+    },
+    landing_uri => {
+        data_type => 'TEXT',
+        is_nullable => 0,
+    },
+    created_on => {
+        data_type => 'DATETIME',
+        set_on_create => 1,
+    },
+    modified_on => {
+        data_type => 'TIMESTAMP',
+        set_on_create => 1,
+        set_on_update => 1,
+    }
+);
+__PACKAGE__->add_unique_constraint( 'unique_id' => [ 'id' ] );
+
+1;
