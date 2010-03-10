daf0cda6f4a0801d01cc994a14d5892827b5c5db
kzhro <kazuhiro.nishikawa@gmail.com>
2010-03-10 16:00:03 +0900

    add schema

---
+++ lib/Lyra/Schema/Result/AdsByArea.pm
@@ -0,0 +1,41 @@
+package Lyra::Schema::Result::AdsByArea;
+use strict;
+use warnings;
+use base qw(Lyra::Schema::Result);
+
+__PACKAGE__->load_components( qw(TimeStamp Core) );
+__PACKAGE__->table('ads_by_area');
+__PACKAGE__->add_columns(
+    id => {
+        data_type => 'CHAR',
+        size      => 10,
+        is_nullable => 0,
+    },
+    title => {
+        data_type => 'VARCHAR',
+        size      => 64,
+    },
+    content => {
+        data_type => 'TEXT',
+    },
+    status  => {
+        data_type => 'INT',
+        is_nullable => 0,
+    },
+    location => {
+        data_type => 'geometry',
+        is_nullable => 0,
+    },
+    created_on => {
+        data_type => 'DATETIME',
+        set_on_create => 1,
+    },
+    modified_on => {
+        data_type => 'TIMESTAMP',
+        set_on_create => 1,
+        set_on_update => 1,
+    }
+);
+__PACKAGE__->add_unique_constraint( 'unique_id' => [ 'id' ] );
+
+1;
