782b82661d1afb75c53e2aa29d749dd8800e8c1f
lestrrat <lestrrat+github@gmail.com>
2010-03-12 14:01:30 +0900

    Tentative script to deploy from master db to searcheable myisam db

---
+++ lib/App/Lyra/DeployAds.pm
@@ -0,0 +1,43 @@
+package App::Lyra::DeployAds;
+use Moose;
+use POSIX ();
+use Lyra::Schema;
+use namespace::autoclean;
+
+with 'MooseX::Getopt';
+
+has dsn => (is => 'ro', isa => 'Str', required => 1);
+has username => (is => 'ro', isa => 'Str');
+has password => (is => 'ro', isa => 'Str');
+
+sub run {
+    my $self = shift;
+
+    my $schema = Lyra::Schema->connect($self->dsn, $self->username, $self->password, { RaiseError => 1, AutoCommit => 1 });
+
+    # Deploy a Dummy Table. This is a bit tricky
+    my $source = $schema->source('AdsByArea');
+
+    my $real_table = $source->from();
+    my $temp_table = POSIX::strftime('ads_by_area_%Y%m%d%H%M%S', localtime);
+    $source->name( $temp_table );
+
+    my $guard = $schema->txn_scope_guard();
+    $schema->deploy(
+        {
+            quote_field_names => 0,
+            sources => [ 'AdsByArea' ]
+        }
+    );
+    $source->name( $real_table );
+
+    my $master_source = $schema->resultset('AdsMaster')->result_source;
+    my $master_table  = $master_source->from;
+
+    my $dbh = $schema->storage->dbh;
+    $dbh->do( "INSERT INTO $temp_table SELECT * FROM $master_table");
+    $dbh->do( "RENAME TABLE $real_table TO ${real_table}_old, $temp_table TO $real_table" );
+    $guard->commit;
+}
+
+1;
