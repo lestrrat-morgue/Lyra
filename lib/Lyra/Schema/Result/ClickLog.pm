8872f21c4700a81eeefdb858801c736bac1e99de
lestrrat <lestrrat+github@gmail.com>
2010-03-16 15:36:16 +0900

    modify clicklog to be a queue

--- lib/Lyra/Schema/Result/ClickLog.pm
+++ lib/Lyra/Schema/Result/ClickLog.pm
@@ -3,28 +3,23 @@ use strict;
 use warnings;
 use base qw(Lyra::Schema::Result);
 
+__PACKAGE__->engine( 'QUEUE' );
 __PACKAGE__->load_components( qw(TimeStamp Core) );
 __PACKAGE__->table('lyra_click_log');
 __PACKAGE__->add_columns(
-    id => {
+    ad_id => {
         data_type => 'CHAR',
-        size      => 10,
+        size      => 36,
         is_nullable => 0,
     },
-    landing_uri => {
+    data => {
         data_type => 'TEXT',
         is_nullable => 0,
     },
     created_on => {
         data_type => 'DATETIME',
         set_on_create => 1,
-    },
-    modified_on => {
-        data_type => 'TIMESTAMP',
-        set_on_create => 1,
-        set_on_update => 1,
     }
 );
-__PACKAGE__->set_primary_key('id');
 
 1;
