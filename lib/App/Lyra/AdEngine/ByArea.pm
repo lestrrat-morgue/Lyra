121777d4f917cb0931482e1a702b5851ca9aaca9
lestrrat <lestrrat+github@gmail.com>
2010-03-16 16:34:59 +0900

    refactor to correctly parse for log-specific options

--- lib/App/Lyra/AdEngine/ByArea.pm
+++ lib/App/Lyra/AdEngine/ByArea.pm
@@ -26,13 +26,63 @@ has password => (
     isa => 'Str',
 );
 
+sub build_log {
+    my ($self, $prefix) = @_;
+
+    my $class = "Lyra::Log::Storage::File";
+    local @ARGV = @{ $self->extra_argv };
+    Getopt::Long::GetOptions( "$prefix-log-class=s" => \$class );
+
+    if ($class !~ s/^\+//) {
+        $class = "Lyra::Log::Storage::$class";
+    }
+
+    if (! Class::MOP::is_class_loaded($class) ) {
+        Class::MOP::load_class($class);
+    }
+
+    my $object;
+    if ( $class->isa('Lyra::Log::Storage::File') ) {
+        my $file_prefix = "adengine_byarea";
+        Getopt::Long::GetOptions( "$prefix-log-prefix=s" => \$file_prefix );
+        $object = $class->new(prefix => $file_prefix);
+    } elsif ( $class->isa('Lyra::Log::Storage::Q4M') ) {
+        my ($dsn, $username, $password, $sql);
+        my $table = "${prefix}_log_queue";
+        Getopt::Long::GetOptions( 
+            "$prefix-log-dsn=s" => \$dsn,
+            "$prefix-log-user=s" => \$username,
+            "$prefix-log-password=s" => \$password,
+            "$prefix-log-table=s" => \$table,
+            "$prefix-log-sql=s" => \$sql,
+        );
+
+        my %args = (
+            dbh => AnyEvent::DBI->new(
+                $dsn,
+                $username,
+                $password,
+                exec_server => 1,
+                RaiseError => 1,
+                AutoCommit => 1,
+            )
+        );
+        $args{table} = $table if defined $table;
+        $args{sql} = $table if defined $table;
+        $object = $class->new(%args);
+    }
+
+    return $object;
+}
+
+
 sub build_app {
     my $self = shift;
 
     # XXX make it possible to change the storage type depending on
     # command line parameters
-    my $request_log = Lyra::Log::Storage::File->new();
-    my $impression_log = Lyra::Log::Storage::File->new();
+    my $request_log = $self->build_log( 'request' );
+    my $impression_log = $self->build_log( 'impression' );
 
     my $dbh = AnyEvent::DBI->new(
         $self->dsn,
@@ -44,6 +94,7 @@ sub build_app {
     );
     Lyra::Server::AdEngine::ByArea->new(
         dbh => $dbh,
+        request_log_storage => $request_log,
         impression_log_storage => $impression_log,
     )->psgi_app;
 }
