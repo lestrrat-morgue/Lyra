d5e11f25439e23f277c4741c0a426b6e8117c82c
lestrrat <lestrrat+github@gmail.com>
2010-03-18 18:39:04 +0900

    docs

--- lib/Lyra/Trait/WithDBI.pm
+++ lib/Lyra/Trait/WithDBI.pm
@@ -30,7 +30,25 @@ Lyra::Trait::WithDBI - Adds Asynchronous DBI Access
 
     sub whatever {
         my $self = shift;
-        $self->cache->exec($sql, @binds, $cb);
+        $self->execsql($sql, @binds, $cb);
     }
+
+
+    MyClass->new(
+        dbh => AnyEvent::DBI->new(
+            $dsn,
+            $username,
+            $password,
+            exec_server => 1, # recommended
+            RaiseError => 1,
+            AutoCommit => 1,
+        )
+    );
+
+=head1 METHODS
+
+=head2 execsql($sql, @binds, $cb->($dbh, $rows, $rv))
+
+Simple delegate to AnyEvent::DBI::exec.
 
 =cut
