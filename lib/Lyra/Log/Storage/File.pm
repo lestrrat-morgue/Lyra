164d0b3d4cbe7bdbe6aaf7fbddf8f0208569fd33
lestrrat <lestrrat+github@gmail.com>
2010-03-18 18:24:35 +0900

    cache the filehandle so that we don't open too many files

--- lib/Lyra/Log/Storage/File.pm
+++ lib/Lyra/Log/Storage/File.pm
@@ -25,24 +25,44 @@ has groups => (
 # filename is <prefix>.<timestamp>.<pid>
 # where timestamp is YYYYMMDDhhXX, and it's recycled every 15 minutes.
 # so on a given hour, you get YYYYMMDDhh01, 02, 03, 04
+
+has filehandles => (
+    is => 'ro',
+    isa => 'HashRef',
+    default => sub { +{} }
+);
+
 sub store {
     my ($self, $message, $cb) = @_;
 
+    # get the current file name we should be writing to
     my @localtime = localtime();
     my $filename = join('.', 
         $self->prefix,
-        POSIX::strftime('%Y%m%d%H', @localtime) . (int($localtime[1] / 15) + 1),
+        sprintf('%s%02d', 
+            POSIX::strftime('%Y%m%d%H', @localtime),
+            int($localtime[1] / 15) + 1
+        ),
         $$,
         'dat',
     );
 
+    my $fh = $self->filehandles->{ $filename };
+
+    if ( ! $fh ) {
+        open($fh, '>>', $filename) or die "Failed to open $filename for writing $!";
+        $self->filehandles->{ $filename } = $fh;
+        my $t; $t = AE::timer 15 * 60, 0, sub {
+            undef $t;
+            delete $self->filehandles->{ $filename };
+        };
+    }
+
     # このCB、本当はいらないんだけど、これがないとテストがうまく書けない・・・
     $cb ||= \&Lyra::_NOOP;
     my $length = length $message;
-
-    aio_open $filename, O_WRONLY|O_CREAT|O_APPEND, 0644, sub {
-        my $fh = shift or die "Failed to  open $filename for writing: $!";
-        aio_write $fh, -1, $length, $message, 0, sub { $cb->($filename) };
+    aio_write $fh, -1, $length, $message, 0, sub { 
+        $cb->($filename);
     };
 }
 
